72f292aab506fd44b0ed136465127e4f88720fdf
Daniels2405 <danielbarrientossalas@hotmail.com>
2026-01-27 22:54:50 -0600

    Primer commit, inicialización del proyecto

---
+++ BusNovaTech/src/main/java/com/mycompany/busnovatech/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.busnovatech;
+
+/**
+ *
+ * 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
