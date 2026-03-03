e789d91551de9c232a38f0638481fff479f388ac
Mario Ortega <138740552+AfroProfessional@users.noreply.github.com>
2026-03-03 09:55:35 -0600

    Add license and author comments in Main.java
    Added license and author comments to Main.java

--- BusNovaTech/src/main/java/com/mycompany/busnovatech/Main.java
+++ BusNovaTech/src/main/java/com/mycompany/busnovatech/Main.java
@@ -1,4 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.busnovatech;
+
+/**
+ *
+ * @author Grupo 1
+ */
 
 import javax.swing.JOptionPane;
 
